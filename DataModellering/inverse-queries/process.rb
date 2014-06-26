428dcf630332f4be1661b1917f531b4005f37c87
Frederic Vogels <frederic.vogels@gmail.com>
2014-06-26 17:16:13 +0200

    DataModellering inverse queries

--- DataModellering/inverse-queries/process.rb
+++ DataModellering/inverse-queries/process.rb
@@ -6,39 +6,58 @@ module Database
   @@db = nil
 
   def self.initialize
-    @@db = SQLite3::Database.new ":memory:"
+#    @@db = SQLite3::Database.new ":memory:"
   end
 
-  def execute(sql)
-    @@db.execute(sql)
+  def self.execute(sql)
+#    @@db.execute(sql)
   end
 
-  def raw_query(sql)
-    @@db.execute2(sql)
+  def self.raw_query(sql)
+#    @@db.execute2(sql)
   end
 
-  def query(sql)
+  def self.query(sql)
     headers, *rows = raw_query(sql)
 
     Table.new(headers, rows)
   end
 
-  def get_table(table_name)
+  def self.get_table(table_name)
     query("SELECT * FROM #{table_name}")
   end
 
-  def drop_table(table_name)
+  def self.drop_table(table_name)
     execute("DROP TABLE #{table_name}")
   end
 end
 
+class String
+  def indent
+    lines.map { |s| "  " + s }.join
+  end
 
-def indent(string)
-  string.lines.map { |s| "  " + s }.join
-end
+  def unindent
+    lines.map { |s| s.strip }.join("\n")
+  end
 
-def unindent(string)
-  string.lines.map { |s| s.strip }.join("\n")
+  def blocks
+    result = []
+    current = ""
+    
+    lines.each do |line|
+      if line.start_with?("#") then
+        result.push(current)
+        current = line
+      else
+        current += line
+      end
+
+      result.push(current)
+
+      result
+    end
+  end
 end
 
 def format_table(headers, rows)
@@ -71,49 +90,64 @@ class Table
   attr_reader :headers, :rows
 end
 
+
+
 class Exercise
-  def self.parse(data)
-    data =~ /#SETUP(.+)#QUERY(.+)$/m or abort "Could not read\n#{indent(data)}"
-    setup, query = $1.strip, unindent($2)
-    table_names = setup.scan(/CREATE TABLE (\w+) \(/).map { |x| x[0] } or abort "Could not find tables in\n#{indent(data)}"
+  # def self.parse(data)
+  #   data =~ /#SETUP(.+)#QUERY(.+)$/m or abort "Could not read\n#{indent(data)}"
+  #   setup, query = $1.strip, unindent($2)
+  #   table_names = setup.scan(/CREATE TABLE (\w+) \(/).map { |x| x[0] } or abort "Could not find tables in\n#{indent(data)}"
 
-    setup.split("\n\n").each do |sql|
-      Database.execute(sql)
-    end
+  #   setup.split("\n\n").each do |sql|
+  #     Database.execute(sql)
+  #   end
 
-    inputTables = table_names.map do |table_name|
-      Database.get_table(table_name)
-    end
+  #   inputTables = table_names.map do |table_name|
+  #     Database.get_table(table_name)
+  #   end
 
-    headers, *rows = Database.query(query)
+  #   headers, *rows = Database.query(query)
 
-    table_names.each do |table_name|
-      Database.drop_table(table_name)
-    end
+  #   table_names.each do |table_name|
+  #     Database.drop_table(table_name)
+  #   end
 
-    Exercise.new(inputTables, query, solutionTable)
+  #   Exercise.new(inputTables, query, solutionTable)
+  # end
+
+  def self.parse(string)
+    p(string.blocks)
   end
 
-  def initialize(tables, query, solution)
-    @tables = tables
-    @query = query
-    @solution = solution
+  def initialize(data)
+    @data = data
   end
 
-  attr_reader :tables, :query, :solution
+  def method_missing(id)
+    @data[name.id2name]
+  end
 end
 
 
 
 def process_file(data)
-  data.scan(/#EXERCISE(.+?)#END/m).map do |exercise|
+  data.scan(/#Exercise(.+?)#End/m).map do |exercise|
     exercise[0].strip
   end.each do |exercise|
-    p Exercise.parse(exercise)
+    Exercise.parse(exercise)
   end
 end
 
 
-process_file(IO.read('inner-joins.txt'))
+# process_file(IO.read('inner-joins.txt'))
 
 
+x = <<END
+#jfklfj jflf
+fjdklf
+fjdkl
+
+#fjlkf fjkl
+END
+
+p(x.blocks.join(" | "))
