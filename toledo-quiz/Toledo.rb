eb7129569f1c2ab98f55e56ae07c47969ce7ed3d
Frederic Vogels <frederic.vogels@gmail.com>
2014-07-10 22:59:01 +0200

    Toledo.rb

--- toledo-quiz/Toledo.rb
+++ toledo-quiz/Toledo.rb
@@ -1,4 +1,134 @@
+require './Types.rb'
+require './Shared.rb'
+
+
 module Toledo
+  class Question
+    protected
+    def join(fields)
+      fields.join("|")
+    end
+  end
+
+  class FillInQuestion < Question
+    PREFIX = 'FIB'
+    
+    def initialize(text)
+      Types.check( binding, {
+                     'text' => String
+                   } )
+
+      @text = text
+      @answers = []
+    end
+
+    def add_answer(answer)
+      @answers << answer
+    end
+
+    def to_s
+      join( [ PREFIX, @text ] + @answers )
+    end
+  end
+
+  class MultipleFillInQuestion < Question
+    PREFIX = 'FIB_PLUS'
+
+    def initialize(text)
+      Types.check( binding, {
+                     'text' => String,                     
+                   } )
+
+      @text = text
+      @answers = []
+    end
+
+    def add_answer(variable, possible_answers)
+      Types.check( binding, {
+                     'variable' => String,
+                     'possible_answers' => [String]
+                   } )
+
+      @answers << [ variable, possible_answers ]
+    end
+
+    def to_s
+      answers = @answers.intersperse([[]]).flatten(1)
+
+      join( [PREFIX, @text ] + answers )
+    end
+  end
+
+  class MultipleAnswerQuestion < Question
+    PREFIX = 'MA'
+
+    def initialize(text)
+      Types.check(binding, {
+                    'text' => String
+                  })
+
+      @text = text
+      @answers = []
+    end
+
+    def add_answer(claim, answer)
+      Types.check( binding, {
+                     'claim' => String,
+                     'answer' => Types.one_of( true, false )
+                   } )
+
+      @answers << [ claim, answer ]
+    end
+
+    def to_s
+      answers = @answers.map do |claim, answer|
+        [ claim, if answer then "correct" else "incorrect" end ]
+      end.flatten(1)
+
+      join([PREFIX, @text ] + answers)
+    end
+  end
+
+  class TrueFalseQuestion < Question
+    PREFIX = 'TF'
+
+    def initialize(text, answer)
+      Types.check( binding, {
+                     'text' => String,
+                     'answer' => Types.one_of(true, false)
+                   } )
+
+      @text = text
+      @answer = answer
+    end
+
+    def to_s
+      answer = if @answer then "waar" else "onwaar" end
+
+      join([PREFIX, @text, answer])
+    end
+  end
+
+  class NumericQuestion < Question
+    PREFIX = 'NUM'
+
+    def initialize(text, answer, delta = 0)
+      Types.check( binding, {
+                     'text' => String,
+                     'answer' => Fixnum,
+                     'delta' => Fixnum
+                   } )
+
+      @text = text
+      @answer = answer
+      @delta = delta
+    end
+
+    def to_s
+      join( [ PREFIX, @text, @answer, @delta ] )
+    end
+  end
+
   def self.generate(questions)
     questions.each_with_index.map do |question, index|
       text = yield index
@@ -7,3 +137,10 @@ module Toledo
     end.join("\n")
   end
 end
+
+
+# q = Toledo::MultipleFillInQuestion.new("Foo")
+# q.add_answer("T1", [ "a", "b" ])
+# q.add_answer("T2", [ "c", "d" ])
+
+# puts q
