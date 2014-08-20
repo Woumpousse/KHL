c9d792a5c6aa91a63cce6dbc79bf2ff023c02908
Merel <merel@alexander.khleuven.be>
2014-08-20 15:49:14 +0200

    Oefening verder aan het afwerken..

--- bop/java/week2-poolpuzzle/doc/Generate.rb
+++ bop/java/week2-poolpuzzle/doc/Generate.rb
@@ -28,12 +28,12 @@ public class VoerUit {
 	int x = 0;
 	while(__vergelijking:x<3__ ) {
 		e1.hello();
-                __e1.add(1)__
-		if(__x<3__) {
+                __:e1.add(1)__
+		if(__vergelijking:x<3__) {
 			e2.add(1);
 		}
-		if(__x>1__) {
-			e2.add(e1.getCount())
+		if(__vergelijking:x>1__) {
+			e2.add(e1.__method:getCount()__)
 		}
 		x++;
 	}
@@ -42,23 +42,23 @@ public class VoerUit {
 }
 public class Echo {
     /* Houdt een getal bij */
-    private int __count__ = 0;
+    private int __variabele naam:count__ = 0;
 
     /* Print enkel hellooo uit */
-    public void __hello__() {
+    public void __methode naam:hello__() {
 	System.out.println("helloooo..");
     }
 
     /* Telt de waarde van i op bij zijn getal */
     public void add(int i) {
-	this.%count = this.%count + %i;
+	this.__variabele naam:count__ = this.__idem:count__ + __parameter:i__;
     }
     public int getCount() {
-	return this.%count;
+	return this.__instantievariabele naam:count__;
     }
     /* Print zijn getal uit */
     public void print() {
-	System.out.println(this.%count);
+	System.out.println(this.__instantievariabele naam:count__);
     }
 }
 END
