95fcbbe640d633fceb82bdfb35b3b7fd92cef792
Frederic Vogels <frederic.vogels@gmail.com>
2014-08-24 15:04:51 +0100

    Algo1 Intro

---
+++ Algo1/topics/intro/code.js
@@ -0,0 +1,2 @@
+var x = parseInt( prompt("Geef een getal") );
+alert("Het kwadraat van " + x + " is gelijk aan " + (x*x));
---
+++ Algo1/topics/intro/example.js
@@ -0,0 +1,6 @@
+function greet()
+{
+    alert("Hello world");
+}
+
+greet();
