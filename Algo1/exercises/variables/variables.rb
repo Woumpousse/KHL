9fef1bf4d262b2d62c644b7920f9ccd24e157bec
Frederic Vogels <frederic.vogels@gmail.com>
2014-08-31 17:17:29 +0100

    Algo1
    Added exercises variables

--- Algo1/exercises/variables/variables.rb
+++ Algo1/exercises/variables/variables.rb
@@ -80,22 +80,16 @@ class Variables < Controller
         x -= 2;
 
         `hide:console.log('x=%d\n', x);`
+
+        var y = 6;
+        y++;
+
+        `hide:console.log('y=%d\n', y);`
       END
     end
   end
 
   def ex7
-    once(__method__) do
-      Questions::JavaScript::InterpretCode.new.parse( <<-'END'.unindent.strip )
-        var x = 6;
-        x++;
-
-        `hide:console.log('x=%d\n', x);`
-      END
-    end
-  end
-
-  def ex8
     once(__method__) do
       Questions::JavaScript::InterpretCode.new.parse( <<-'END'.unindent.strip )
         var x = 0;
@@ -109,6 +103,60 @@ class Variables < Controller
         `hide:console.log('x=%d\n', x);`
         `hide:console.log('y=%d\n', y);`
         `hide:console.log('z=%d\n', z);`
+      END
+    end
+  end
+
+  def ex8
+    once(__method__) do
+      Questions::JavaScript::InterpretCode.new.parse( <<-'END'.unindent.strip )
+        var a = true;
+        var b = false;
+        var c = a || b;
+        var d = a && b;
+        var e = !a;
+
+        `hide:console.log('a=%s\n', a ? 'true' : 'false');`
+        `hide:console.log('b=%s\n', b ? 'true' : 'false');`
+        `hide:console.log('c=%s\n', c ? 'true' : 'false');`
+        `hide:console.log('d=%s\n', d ? 'true' : 'false');`
+        `hide:console.log('e=%s\n', e ? 'true' : 'false');`
+      END
+    end
+  end
+
+  def ex9
+    once(__method__) do
+      Questions::JavaScript::InterpretCode.new.parse( <<-'END'.unindent.strip )
+        var a = 5;
+        var b = 8;
+        var c = a < b;
+        var d = b !== 8;
+        var e = c || d || (123+456*987 === 444 + 561 * 2);
+
+        `hide:console.log('a=%d\n', a);`
+        `hide:console.log('b=%d\n', b);`
+        `hide:console.log('c=%s\n', c ? 'true' : 'false');`
+        `hide:console.log('d=%s\n', d ? 'true' : 'false');`
+        `hide:console.log('e=%s\n', e ? 'true' : 'false');`
+      END
+    end
+  end
+
+  def ex10
+    once(__method__) do
+      Questions::JavaScript::InterpretCode.new.parse( <<-'END'.unindent.strip )
+        var a = 5
+        var b = true;
+        var c = "abc";
+        var d = a === b;
+        var e = b !== c;
+
+        `hide:console.log('a=%d\n', a);`
+        `hide:console.log('b=%d\n', b);`
+        `hide:console.log('c=%s\n', c);`
+        `hide:console.log('d=%s\n', d ? 'true' : 'false');`
+        `hide:console.log('e=%s\n', e ? 'true' : 'false');`
       END
     end
   end
