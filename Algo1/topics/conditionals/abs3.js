48565e45f178a0b45f236adb1df35cbf6886efa2
Frederic Vogels <frederic.vogels@gmail.com>
2014-09-01 18:30:58 +0100

    Algo1
    Topic Conditionals

---
+++ Algo1/topics/conditionals/abs3.js
@@ -0,0 +1,12 @@
+var x = ???;
+var abs;
+
+if ( x > 0 ) {
+  abs = x;
+}
+else if ( x < 0 ) {
+  abs = -x;
+}
+else
+  abs = x;
+}
---
+++ Algo1/topics/conditionals/abs4.js
@@ -0,0 +1,9 @@
+var x = ???;
+var abs;
+
+if ( x > 0 || x === 0 ) {
+  abs = x;
+}
+else if ( x < 0 ) {
+  abs = -x;
+}
