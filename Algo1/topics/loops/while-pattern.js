31eb7d123c25e20928d4ab257e15cee7d5af227e
Frederic Vogels <frederic.vogels@gmail.com>
2014-09-03 22:41:52 +0100

    Algo1
    Topic loops

---
+++ Algo1/topics/loops/while-pattern.js
@@ -0,0 +1,8 @@
+var `\NODE{i = 0}{init}`;
+
+while ( `\NODE{i < imax}{cond}` )
+{
+    ...
+
+    `\NODE{i++;}{inc}`
+}
---
+++ Algo1/topics/loops/while-pattern2.js
@@ -0,0 +1,5 @@
+`\PLACEHOLDER{init}`
+while ( `\PLACEHOLDER{cond}` ) {
+    `\PLACEHOLDER{body}`
+    `\PLACEHOLDER{inc}`
+}
