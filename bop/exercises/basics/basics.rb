2b0e40bc84266cda06cf9b3f3e505819eed3c5af
Frederic Vogels <frederic.vogels@gmail.com>
2014-09-13 11:10:29 +0100

    BOP
    Basics exercise

--- bop/exercises/basics/basics.rb
+++ bop/exercises/basics/basics.rb
@@ -4,5 +4,15 @@ require './Shared.rb'
 require './Parameters.rb'
 
 class Resources < Controller
-
+  def hello_world
+    Questions::Java::FillInBlanks.new.parse( <<-'END'.unindent.strip )
+        __`class`__ HelloWorld
+        {
+            public static void __`main`__(String[] args)
+            {
+                System.out.__`println`__("__`Hello World`__");
+            }
+        }
+      END
+  end
 end
