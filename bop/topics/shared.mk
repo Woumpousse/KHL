53cb6d7f7641f0aca1b90c3c307060eb60b170c4
Frederic Vogels <frederic.vogels@gmail.com>
2014-09-15 13:26:28 +0100

    BOP
    makefiles

---
+++ bop/topics/shared.mk
@@ -0,0 +1,24 @@
+.PHONY: upload
+
+ifeq ($(OS),Windows_NT)
+SCP=scp2 -P 22345 -W g:/pw
+SSH=ssh2 -p 22345 -W g:/pw
+else
+SCP=scp -P 22345 -W ~/pw
+SSH=ssh -p 22345 -W ~/pw
+endif
+
+LOGIN = u0057764
+URL = alexander.khleuven.be
+REMOTE_DIRECTORY = /var/www/tw1/slides
+
+
+%.pdf: %.tex
+	pdflatex $<
+	pdflatex $<
+
+upload: $(CURRENT).pdf
+	$(SCP) $(CURRENT).pdf $(LOGIN)@$(URL):$(REMOTE_DIRECTORY)
+
+clean_tex:
+	rm -f *.pdf *.out *.snm *.log *.toc *.nav *~
