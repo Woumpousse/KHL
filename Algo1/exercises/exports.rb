72f81a650ad8c6f5f654db0303df3c71bb1d94de
Frederic Vogels <frederic.vogels@gmail.com>
2014-09-19 22:49:25 +0100

    Algo1
    Cleanup

--- Algo1/exercises/exports.rb
+++
@@ -1,9 +0,0 @@
-ARGV.each do |dir|
-  Dir.chdir( dir ) do
-    result = IO.readlines('export').each do |pattern|
-      Dir[ pattern.strip ].each do |entry|
-        puts "#{dir}/#{entry}"
-      end
-    end
-  end
-end
