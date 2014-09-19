639ca81a0db758014e1f0520cf8d401fcd45a7dd
Frederic Vogels <frederic.vogels@gmail.com>
2014-09-19 08:24:16 +0100

    Makefile

---
+++ shared.mk
@@ -0,0 +1,20 @@
+PORT = 22345
+
+ifeq ($(OS),Windows_NT)
+SCP=scp2 -P $(PORT) -W g:/pw
+SSH=ssh2 -p $(PORT) -W g:/pw
+ZIP = 7z a
+else
+SCP=scp -P $(PORT) -W ~/pw
+SSH=ssh -p $(PORT) -W ~/pw
+ZIP = zip
+endif
+
+URL = alexander.khleuven.be
+ROOT_REMOTE_DIRECTORY = /var/www
+
+# $(call UPLOAD,file,directory)
+UPLOAD=$(SCP) $(1) $(KHL_LOGIN)@$(URL):$(ROOT_REMOTE_DIRECTORY)/$(2)
+
+# $(call REMOTE_EXECUTE,command)
+REMOTE_EXECUTE=$(SSH) $(1)
